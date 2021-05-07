5357e45eacaf2ec4c1f4976873f764cd6fc2e407
Pradyumna Hebbar <pradyu.hebbar@gmail.com>
2021-05-07 09:55:08 +0530

    Initial Commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+  ]);
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
